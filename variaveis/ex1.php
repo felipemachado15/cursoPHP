15cfc6fbb7f96d72e2a918b1fd587ebb6a4a56e3
Felipe Machado <felipe.machado.ads@gmail.com>
2019-02-25 15:19:37 -0300

    Aula 1 váriaveis em PHP

---
+++ variaveis/ex1.php
@@ -0,0 +1,15 @@
+
+
+<?php
+//váriavel = espaço na memoria para popular valores
+
+//$nome : o $ indica que é uma váriavel
+$nome = "Hcode"; //String aspas duplas ou simples
+//echo $nome ; impressão na tela
+
+var_dump($nome); // informações variaveis ,tipo,valor e tamanho.
+
+
+
+
+?>
